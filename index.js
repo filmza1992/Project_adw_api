2b1a0d982777f895629260fb8cb9a7cc695f91b9
filmza1992 <ddza1992@hotmail.com>
2024-03-27 21:07:58 +0700

    new version for ngrok

--- index.js
+++ index.js
@@ -1,53 +1,64 @@
 require('dotenv').config();
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const oas3Tools = require('oas3-tools');
+
+const serverPort = process.env.PORT;
 const cors = require('cors');
 
-const serverPort = process.env.PORT || 3000; // Use port 3000 if PORT environment variable is not set
 const mongoString = process.env.DATABASE_URL;
 
-// Connect to MongoDB
-mongoose.connect(mongoString, {
-  dbName: 'Project_adw',
+// contect to mongo
+mongoose.connect(mongoString,{
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  dbName: 'Project_adw'
 });
 const database = mongoose.connection;
 
-// Handle MongoDB connection errors
-database.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-database.once('open', () => {
-  console.log('Database Connected');
-});
-
-// SwaggerRouter configuration
+// swaggerRouter configuration
 const options = {
   routing: {
     controllers: path.join(__dirname, './controllers'),
   },
 };
 
-// Initialize oas3Tools middleware
-const openApiAppConfig = oas3Tools.expressAppConfig(
-  path.join(__dirname, 'api/openapi.yaml'),
-  options
-);
-const openApiApp = openApiAppConfig.getApp();
+const expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+const openApiApp = expressAppConfig.getApp();
 
-// Create Express app
 const app = express();
+app.use(/.*/, cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  if (req.method === 'OPTIONS') {
+    res.status(204).send();
+  } else {
+    next();
+  }
+});
 
-// Middleware
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+for (let i = 2; i < openApiApp._router.stack.length; i++) {
+  app._router.stack.push(openApiApp._router.stack[i]);
+}
 
-// Use oas3Tools middleware
-app.use(openApiApp); // Use the OpenAPI app
+// checking conect
+database.on('error', (error) => {
+  console.log(error);
+});
 
-// Export the app for Cyclic
-module.exports = app;
+database.once('connected', () => {
+  console.log('Database Connected');
+});
+
+// Initialize the Swagger middleware
+http.createServer(app).listen(serverPort, () => {
+  console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+  console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+});
